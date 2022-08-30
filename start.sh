655e7cb4372b922ba67dfb656e91d5df4c1ce565
Serge Voilokov <serge0x76@gmail.com>
2022-08-30 14:57:51 -0400

    Update deployment

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 ssh my <<EOF
-    docker kill localhost/cooking
-    docker rm localhost/cooking
+    docker kill cooking
+    docker image rm localhost/cooking
 EOF
 
 gzip -c cooking.img | ssh my 'gunzip | docker load'
